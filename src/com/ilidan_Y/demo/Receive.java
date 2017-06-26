7a3c51e6d83608ab4f775419132f9f84b84bdd34
yangyang <smile_yangy@163.com>
2017-06-26 11:26:26 +0800

    change method server

--- src/com/ilidan_Y/demo/Receive.java
+++ src/com/ilidan_Y/demo/Receive.java
@@ -3,10 +3,14 @@ package com.ilidan_Y.demo;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.ShutdownSignalException;
 
@@ -18,7 +22,7 @@ import com.rabbitmq.client.ShutdownSignalException;
  */
 public class Receive {
 	// 队列名称
-	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_NAME = "helloMap";
 
 	public static void main(String[] args) throws IOException, TimeoutException, ShutdownSignalException,
 			ConsumerCancelledException, InterruptedException {
@@ -28,18 +32,31 @@ public class Receive {
 		Connection connection = connectionFactory.newConnection();
 		Channel channel = connection.createChannel();
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-		System.out.println("消息等待中。。。");
+		System.out.println("waiting for message...");
 
 		// 创建队列消费者
-		QueueingConsumer consumer = new QueueingConsumer(channel);
+		//QueueingConsumer consumer = new QueueingConsumer(channel);
 		// 指定消费队列
+//		channel.basicConsume(QUEUE_NAME, true, consumer);
+//		while (true) {
+//			// nextDelivery是一个阻塞方法（内部实现其实是阻塞队列的take方法）
+//			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+//			String message = new String(delivery.getBody());
+//			System.out.println(" [x] Received '" + message + "'");
+//		}
+		
+		//创建消费者
+//      DefaultConsumer类实现了Consumer接口，通过传入一个频道，告诉服务器我们需要那个频道的消息，如果频道中有消息，就会执行回调函数handleDelivery 
+		Consumer consumer = new DefaultConsumer(channel) {
+			@Override
+			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties,
+					byte[] body) throws IOException {
+				String message = new String(body, "UTF-8");
+				System.out.println("Received '" + message + "'");
+			}
+		};
+//      自动回复队列应答
 		channel.basicConsume(QUEUE_NAME, true, consumer);
-		while (true) {
-			// nextDelivery是一个阻塞方法（内部实现其实是阻塞队列的take方法）
-			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-			String message = new String(delivery.getBody());
-			System.out.println(" [x] Received '" + message + "'");
-		}
 
 	}
 
--- src/com/ilidan_Y/demo/Send.java
+++ src/com/ilidan_Y/demo/Send.java
@@ -1,8 +1,11 @@
 package com.ilidan_Y.demo;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeoutException;
 
+import com.alibaba.fastjson.JSON;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
@@ -16,9 +19,11 @@ public class Send {
 	
 	
 	
-	private static final String QUEUE_NAME = "hello";
+	private static final String QUEUE_NAME = "helloMap";
 
 	public static void main(String[] args) throws IOException, TimeoutException {
+		//创建消息体
+		Map<String,String> map = new HashMap<String,String>();
 		//创建连接工厂
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		//设置主机地址
@@ -30,10 +35,12 @@ public class Send {
 		//制定一个消息队列
 		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 		//发送的消息  
-        String message = "hello world!";  
+        //String message = "hello world 123!"; 
+        map.put("m1", "hello world!");
+        map.put("m2", "hello world 123!");
         //往队列中发出一条消息  
-        channel.basicPublish("", QUEUE_NAME, null, message.getBytes());  
-        System.out.println(" send '" + message + "'");  
+        channel.basicPublish("", QUEUE_NAME, null,JSON.toJSONString(map).getBytes());  
+        System.out.println(" send '" + map + "'");  
         //关闭频道和连接  
         channel.close();  
         connection.close(); 
